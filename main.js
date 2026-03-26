a0d2d4015811320af7abb0b832f8f6f97a1b463a
ktynerdev <ktyner.dev@gmail.com>
2026-03-26 15:21:51 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,31 +1,17 @@
 import { BootScene } from "./scenes/BootScene.js";
 import { MenuScene } from "./scenes/MenuScene.js";
-import { HowToPlayScene } from "./scenes/HowToPlayScene.js";
-import { StatsScene } from "./scenes/StatsScene.js";
-import { GameScene } from "./scenes/GameScene.js";
-import { ResultsScene } from "./scenes/ResultsScene.js";
 
 const config = {
   type: Phaser.AUTO,
   parent: "game-container",
   width: 540,
   height: 960,
-  backgroundColor: "#071120",
+  backgroundColor: "#08111f",
   scale: {
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
   },
-  input: {
-    activePointers: 3,
-  },
-  scene: [
-    BootScene,
-    MenuScene,
-    HowToPlayScene,
-    StatsScene,
-    GameScene,
-    ResultsScene,
-  ],
+  scene: [BootScene, MenuScene],
 };
 
 new Phaser.Game(config);
